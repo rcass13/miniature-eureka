6aa1c68f2c43d52c8e4b5f6186f952ce8ffdbd8f
RoseCass <rosecassidy1303@gmail.com>
2023-01-19 18:08:19 -0800

    removed Develop file

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 //dependencies
 const fs = require('fs');
 const util = require('util');
-
-const path = require('path');
 const apiRoutes = require ('./routes/api.routes')
-const htmlRoutes = require ('./routes/html.routes')
-
+const htmlRoutes = require ('./routes/html.routes');
 //set up express
 const express = require ('express')
 const app = express();
@@ -13,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 
 // Helper method for generating unique ids
-const uuid = require('./helpers/uuid'); 
+const uuid = require('./helpers/uuid')
 // const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 
@@ -23,30 +20,10 @@ app.use(express.static('public'));
 
 
 //middleware parsing JSON
-// app.use(require('./routes'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-
-// // GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
-
-// // GET Route for retrieving all the tips
-// app.get('/api/notes', (req, res) => {
-//   fs.readFile('./db/db.json','utf8', (err, data) => {
-//       if (err) {
-//         console.error(err);
-//       } else {
-//           const parsedData = JSON.parse(data);
-//           res.json(parsedData);
-//           //   parsedData.push(content);
-//           //   writeToFile(file, parsedData);
-//       }
-//     });
-// });
 
 // PORT
 
